f33c7742ba34fa374d089a1fc7221b38f792f194
stephlane544 <stephlane544@gmail.com>
2019-08-03 14:37:14 -0600

    part of function complete

--- coding-challenge.js
+++ coding-challenge.js
@@ -10,8 +10,19 @@
 
 
 function packageDependencies(arr){
+    let packageArr = [];
+    let dependencyArr = [];
+    let outputString = 'Valid';
+
     for(let i = 0; i < arr.length; i++){
-        arr[i]
+        let newArr = arr[i].split(" ");
+        outputString = newArr.length > 2 ? 'Invalid - Each package must have at most one dependency' : outputString;
+        packageArr.push(newArr[0]);
+        dependencyArr.push(newArr[1]);
+    }
+
+    while(outputString === "Valid"){
+        
     }
 }
 
