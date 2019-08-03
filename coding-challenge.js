f9da919334978e92108756c19b9c5b74c6c861ba
stephlane544 <stephlane544@gmail.com>
2019-08-03 15:11:01 -0600

    added to function

--- coding-challenge.js
+++ coding-challenge.js
@@ -17,12 +17,29 @@ function packageDependencies(arr){
     for(let i = 0; i < arr.length; i++){
         let newArr = arr[i].split(" ");
         outputString = newArr.length > 2 ? 'Invalid - Each package must have at most one dependency' : outputString;
+        if (newArr.length > 2){
+            outputString = 'Invalid - Each package must have at most one dependency'
+        } else if (newArr[0][newArr.length -1] != ':' || newArr[0].indexOf(':') != newArr[0].length -1){
+            outputString = 'Invalid - Each package must have a colon and space after, even if it has to dependencies to list'
+        }
+        let firstElement = newArr[0].pop();
+        debug(firstElement);
         packageArr.push(newArr[0]);
         dependencyArr.push(newArr[1]);
     }
 
     while(outputString === "Valid"){
-        
+        let index = 0;
+        let firstPackage;
+        for(let i = 0; i < dependencyArr.length; i++){
+            if (index = i){
+                for(let y = 0; y < packageArr.length; y++){
+                    if(packageArr[y] === dependencyArr[i]){
+
+                    }
+                }
+            }
+        }
     }
 }
 
