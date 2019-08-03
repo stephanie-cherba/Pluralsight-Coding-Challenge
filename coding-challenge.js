ba477a4d02fb7a9da11fe9756d26e74932eda820
stephlane544 <stephlane544@gmail.com>
2019-08-03 13:52:11 -0600

    initial commit

---
+++ coding-challenge.js
@@ -0,0 +1,10 @@
+// example input 
+// [
+//  "KittenService: ",
+//  "Leetmeme: Cyberportal",
+//  "Cyberportal: Ice",
+//  "CamelCaser: KittenService",
+//  "Fraudstream: Leetmeme",
+//  "Ice: "
+// ]
+
