87d2fc1ed3d000516c52d32681f4f6ffb6d65bd6
stephlane544 <stephlane544@gmail.com>
2019-08-03 19:48:56 -0600

    finished logic

--- coding-challenge.js
+++ coding-challenge.js
@@ -19,32 +19,60 @@ function packageDependencies(arr){
     //loop through the array passed into the function, splitting each string into the separate packages and dependencies and double check that there are only 2 dependencies and that each package has a colon right after it.
     for(let i = 0; i < arr.length; i++){
         let newArr = arr[i].split(" ");
-        outputString = newArr.length > 2 ? 'Invalid - Each package must have at most one dependency' : outputString;
         if (newArr.length > 2){
             outputString = 'Invalid - Each package must have at most one dependency'
         } else if (newArr[0].indexOf(':') != newArr[0].length -1){
             outputString = 'Invalid - Each package must have a colon and space after, even if it has to dependencies to list'
+        } else {
+            let firstElement = newArr[0].replace(":", "");
+            firstElement = firstElement.replace(" ", "");
+            packageArr.push(firstElement);
+            dependencyArr.push(newArr[1]);
         }
-        let firstElement = newArr[0].replace(":", "");
-        firstElement = firstElement.replace(" ", "");
-        packageArr.push(firstElement);
-        dependencyArr.push(newArr[1]);
     }
 
     //loop through the array of dependencies. Add the packages with no dependencies to the output array first.
-    while(outputString === "Valid"){
-        console.log('here')
-        let index = 0;
-        let firstPackage;
+    while(outputArr.length < dependencyArr.length){
         for(let i = 0; i < dependencyArr.length; i++){
-            if (index = i){
-                for(let y = 0; y < packageArr.length; y++){
-                    if(packageArr[y] === dependencyArr[i]){
+            let currentPackage = packageArr[i]
+            if (dependencyArr[i] === ''){
+                let containsPackage = false;
+                for(let j = 0; j < outputArr.length; j++){
+                    if(outputArr[j] == currentPackage){
+                        containsPackage = true;
+                    }
+                }
+                if(containsPackage != true){
+                outputArr.push(currentPackage);
 
+                }
+            }
+        }
+        for(let k = 0; k < dependencyArr.length; k++){
+            let currentPackage = packageArr[k]
+            if (dependencyArr[k] != ''){
+                for(let k = 0; k < packageArr.length; k++){
+                    if(dependencyArr[k] == packageArr[k]){
+                        outputArr.push(packageArr[k]);
+                    }
+                }
+                let containsPackage = false;
+                let containsDependency = false;
+                for(let l = 0; l < outputArr.length; l++){
+                    if(outputArr[l] == currentPackage){
+                        containsPackage = true;
+                    }
+                    if(outputArr[l] == dependencyArr[k]){
+                        containsDependency = true;
+                        outputArr.push(packageArr[k])
+                        console.log(outputArr)
                     }
                 }
             }
         }
+    }
+    if(outputArr.length > 0){
+        outputString = outputArr.toString(",  ");
     }
     return outputString;
 }
